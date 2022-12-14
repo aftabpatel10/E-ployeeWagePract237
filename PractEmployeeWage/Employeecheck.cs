5e9942a628c5afdd84f44ff13d96660b2ec6c5f5
Patel <aftabkpatel10@gmail.com>
2022-12-14 11:47:19 +0530

    Cheking Employee Using Switch Case

--- PractEmployeeWage/Employeecheck.cs
+++ PractEmployeeWage/Employeecheck.cs
@@ -10,8 +10,8 @@ namespace PractEmployeeWage
     {
         public static void checkattendence ()
         {
-            int IS_PART_TIME = 1;
-            int IS_FULL_TIME = 2;
+            const int IS_PART_TIME = 1;
+            const int IS_FULL_TIME = 2;
             int EMP_RATE_PER_HR = 20;
 
             int empHrs = 0;
@@ -19,22 +19,21 @@ namespace PractEmployeeWage
 
             Random random= new Random();
             int empcheck =random.Next(0 , 3);
-
-            if(empcheck == IS_FULL_TIME )
+            switch (empcheck)
             {
-                Console.WriteLine("Employee is Present ");
-                empHrs = 8;
+                case IS_FULL_TIME:
+                    Console.WriteLine("Employee is Present ");
+                    empHrs = 8;
+                    break;
+                case IS_PART_TIME:
+                    Console.WriteLine("Employee is Part Time");
+                    empHrs = 4;
+                    break;
+                default:
+                    Console.WriteLine("Eployee is Absent");
+                    break;
             }
-            else if(empcheck ==IS_PART_TIME)
-            {
-                Console.WriteLine("Employee is Part Time");
-                empHrs = 4;
-            }
-            else
-            {
-                Console.WriteLine("Eployee is Absent");
-            }
-         empWage= EMP_RATE_PER_HR*empHrs;
+            empWage= EMP_RATE_PER_HR*empHrs;
             Console.WriteLine("EmpWage :" + empWage);
         }
         
